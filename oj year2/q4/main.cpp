012e6c23ec84099481871e41af4710c333ff9668
imlone1y <136362929+imlone1y@users.noreply.github.com>
2023-10-28 15:14:20 +0800

    Add files via upload

---
+++ oj year2/q4/main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <cstring>
+
+#define SIZE 100
+
+using namespace std;
+
+class Queue
+{
+public:
+    Queue()
+    {
+        top = 0;
+        bot = 0;
+    }
+    int enqueue(int data)
+    {
+      if((bot+1)%SIZE==top)
+      {
+        //cout<<"top="<<top<<" bot="<<bot<<endl;
+        return -1;
+      }
+      else
+      {
+        queue[bot]=data;
+        bot++;
+        if(bot==SIZE)
+          bot=0;
+        //cout<<"top="<<top<<" bot="<<bot<<endl;
+        return 1;
+      }
+
+    }
+    int *dequeue()
+    {
+      if(top==bot)
+      {
+        //cout<<"top="<<top<<" bot="<<bot<<endl;
+        return NULL;
+      }
+      else
+      {
+        int *p=&queue[top];
+        top++;
+        if(top==SIZE)
+          top=0;
+        //cout<<"top="<<top<<" bot="<<bot<<endl;
+        return p;
+      }
+    }
+private:
+    int queue[SIZE];
+    int top, bot;
+};
+
+int main()
+{
+    int data, *temp;
+    char command[50];
+    Queue *queue = new Queue();
+    while(1)
+    {
+        cin>>command;
+        if(strcmp(command, "exit") == 0)
+        {
+            break;
+        }
+        else if(strcmp(command, "enqueue") == 0)
+        {
+            cout<<"Please input a integer data:";
+            cin>>data;
+            if(queue->enqueue(data) == 1)
+            {
+                cout<<"Successfully enqueue data "<<data<<" into queue."<<endl;
+            }
+            else
+            {
+                cout<<"Failed to enqueue data into queue."<<endl;
+            }
+        }
+        else if(strcmp(command, "dequeue") == 0)
+        {
+            temp = queue->dequeue();
+            if(temp == NULL)
+            {
+                cout<<"Failed to dequeue a data from queue.\n";
+            }
+            else
+            {
+                cout<<"Dequeue data "<<*temp<<" from queue."<<endl;
+            }
+        }
+    }
+}
