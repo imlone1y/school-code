e0ff840f4ca12ea303a18687821f9d57c9e79dd3
imlone1y <136362929+imlone1y@users.noreply.github.com>
2023-10-28 14:58:35 +0800

    Add files via upload

---
+++ oj year2/q2/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+template<class T>
+
+class Memory
+{
+public:
+    static T **allocArray(int m, int n)
+    {
+        T *arr = new T[m*n];
+        T **arr2 = new T*[m];
+        
+        for (int i = 0; i < m; i++)
+        {
+            arr2[i] = arr;
+            arr += n;
+        }
+        return arr2;
+    }
+};
+
+int main()
+{
+    int **array;
+    array = Memory<int>::allocArray(5, 10);
+    int j, k;
+    for(j = 0; j < 5; j++)
+        for(k = 0; k < 10; k++)
+            array[j][k] = j * 10 + k;
+    
+    for(j = 0; j < 5; j++)
+        for(k = 0; k < 10; k++)
+            cout << array[j][k] << " ";
+    
+    return 0;
+}
